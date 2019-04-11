8e67dedc94c45cf2635d07e41a4df6b8e60d800b
numberoneecsstudent <32531002+numberoneecsstudent@users.noreply.github.com>
2019-04-11 13:57:07 -0700

    mvp

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -20,12 +20,12 @@ class TabLink {
     const links = document.querySelectorAll('.tabs-link');
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     // Array.from(links).forEach();
-    
-
+    links.forEach(link => link.classList.remove('tabs-link-selected'));
     // Add a class named "tabs-link-selected" to this link
     // this.element;
-    
+    this.element.classList.add('tabs-link-selected');
     // Call the select method on the item associated with this link
+    this.tabItems.select();
 
   }
 }
@@ -33,17 +33,16 @@ class TabLink {
 class TabItem {
   constructor(element) {
     // Assign this.element to the passed in element
-    // this.element;
+    this.element = element;
   }
 
   select() {
     // Select all ".tabs-item" elements from the DOM
-    // const items;
-
+    const items = document.querySelectorAll('.tabs-item');
     // Remove the class "tabs-item-selected" from each element
-    
+    items.forEach(itemElement => itemElement.remove('tabs-items-selected'));
     // Add a class named "tabs-item-selected" to this element
-    //this.element;
+    this.element.classList.add('tabs-items-selected');
   }
 }
 
@@ -57,4 +56,6 @@ class TabItem {
 
 */
 
-links = document.querySelectorAll();
+const links = document.querySelectorAll('.tabs-link');
+links = Array.from(links).map( link => new TabLink(link));
+links[0].select();
